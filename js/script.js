17db8fff44978b666e44832c3f0ee8b4037e2c7a
Robert Bagwell <erbagwell@yahoo.com>
2020-10-16 23:27:19 -0600

    ADD JS

--- js/script.js
+++ js/script.js
@@ -33,9 +33,71 @@ const words = [
 let randomWord;
 let score = 0;
 let time = 10;
+let difficulty = localStorage.getItem('difficulty') !== null ? localStorage.getItem('difficulty') : 'easy';
+
+diffcultySelect.value = difficulty;
+
+const timeInterval = setInterval(updateTime, 1000);
+
+function updateTime(){
+    time--;
+    timeEl.textContent = time + 's';
+    if(time === 0){
+        clearInterval(timeInterval);
+        gameOver();
+    }
+}
+
+function gameOver(){
+    endGameContainer.innerHTML = `
+        <h1>Time Ran Out!</h>
+        <p>Your Final Score Is ${score}</p>
+        <button onclick="location.reload()">Resart Game</button>
+    `;
+
+    endGameContainer.style.display = 'flex';
+}
 
 function getRandomWord(){
     return words[Math.floor(Math.random() * words.length)];
 }
 
-console.log(getRandomWord());
+function addWordToDOM(){
+    randomWord = getRandomWord();
+    word.textContent = randomWord;
+}
+
+function updeateScore(){
+    score++;
+    scoreEl.textContent = score;
+}
+
+text.addEventListener('input', e=>{
+    const insertedText = e.target.value;
+    if(insertedText === randomWord){
+        addWordToDOM();
+        updeateScore();
+        e.target.value = '';
+
+        if(difficulty === 'hard'){
+            time += 2;
+        } else if (difficulty === 'medium'){
+            time += 3;
+        } else {
+            time += 5;
+        }
+        updateTime();
+    }
+});
+
+settingsBtn.addEventListener('click', ()=>{
+    settings.classList.toggle('hide'); 
+});
+
+settingForm.addEventListener('change', e=>{
+    difficulty = e.target.value;
+    localStorage.setItem('difficulty', difficulty);
+})
+
+addWordToDOM();
+text.focus();
